53cf0ffc82e90e3962159eb0f487c63ed0216390
Jacob Ford <j@cobford.com>
2021-06-15 18:40:26 -0400

    Name package

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
 import { getPieceBySlug, getAllSlugs } from '../lib/api'
 
-export default function TableOfContents() {
+export default function TableOfContents({ pieces }) {
   return (
     <article>
       <h1 className="site-title">Documentation of Jacob Ford</h1>
       <h2 className="site-subtitle">All substance, no style</h2>
       <ul>
-      {getAllSlugs().forEach(pieceSlug => {
-        const piece = getPieceBySlug(pieceSlug)
-        return <li><a href="/pieces/{piece.slug}">{piece.title}</a></li>
-      })}
+      {pieces.map(piece =>
+        <li><a href="/pieces/{piece.slug}">{piece.title}</a></li>
+      )}
       </ul>
     </article>
   )
+}
+
+export async function getStaticProps(context) {
+  const allSlugs = await getAllSlugs()
+  const fields = ['title', 'slug']
+  const allPieces = allSlugs.map(pieceSlug => getPieceBySlug(pieceSlug, fields))
+
+  console.log(allPieces)
+
+  return {
+    props: {
+      pieces: allPieces
+    },
+  }
 }
