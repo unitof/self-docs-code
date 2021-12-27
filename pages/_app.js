946e49cf30bb5fbdd5f870cce21125b38ee3f77f
Jacob Ford <j@cobford.com>
2021-12-27 12:39:20 -0500

    Add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/global.css'
+import { useEffect } from 'react';
+import * as Fathom from 'fathom-client'
 
-function SelfDocsApp({ Component, pageProps }) {
+const fathomTrackingCode = process.env.FATHOM_TRACKING_CODE
+
+function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load(fathomTrackingCode, {
+      includedDomains: [
+        'docs.jacobford.com',
+      ],
+    })
+  })
+
   return <Component {...pageProps} />
 }
 
-export default SelfDocsApp
+export default App
