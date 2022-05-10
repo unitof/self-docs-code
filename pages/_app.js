f9a9e5f81fb4e044b5c7a6eed8f720bd18a83c0a
Jacob Ford <j@cobford.com>
2022-05-10 18:09:34 -0400

    Add Axiom logging

--- pages/_app.js
+++ pages/_app.js
@@ -18,4 +18,29 @@ function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
+// Axiom Web Vitals unsampled logging
+export function reportWebVitals(metric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT
+
+  if (!url) {
+    return
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  })
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, {
+      body,
+      method:
+      'POST',
+      keepalive: true
+    })
+  }
+}
+
 export default App
